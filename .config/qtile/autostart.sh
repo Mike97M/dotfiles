911946044cf67b4248fe0ed064e8055135cad8e5
Mike97M <mike97m@yahoo.com>
2021-01-14 13:28:32 +0200

    qtile config

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# network applet
+nm-applet &
+# systray battery icon
+cbatticon -u 5 &
+# systray volume
+volumeicon &
+# dropbox to sync org notes
+dropbox &
+# screenshot tool
+flameshot &
+# connection with phone
+kdeconnect-indicator &
+# night colors changes
+redshift &
+# regular eye breaks
+safeeyes &
+# polkit
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# for ssh in termite
+export TERM="xterm-256color"
