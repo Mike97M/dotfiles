7419edcddcb73ae7cb41481a110a31c4dc06f558
mike97m <mike97m@yahoo.com>
2022-03-24 23:07:34 +0400

    add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_user_key_bindings
+bind \cf tmux-sessionizer
+end
+zoxide init fish | source
